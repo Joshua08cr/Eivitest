d31a03cb64a2ca1349b3f8b8d56230c73e774f4e
dababy110 <vargasariasdavid110@gmail.com>
2023-08-25 00:16:53 +0200

    25 agosto

--- screen/HomeScreen.jsx
+++ screen/HomeScreen.jsx
@@ -3,11 +3,11 @@ import {
   StyleSheet,
   View,
   Text,
-  Image,
   Dimensions,
   TouchableOpacity,
-  ImageBackground,
 } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+
 import { useNavigation } from "@react-navigation/native";
 
 
@@ -16,39 +16,48 @@ import { useNavigation } from "@react-navigation/native";
 const { width } = Dimensions.get("window");
 
 
+const StripedBackground = ({ children }) => {
+  return (
+    <View style={styles.stripedBackground}>
+      {children}
+      <View style={styles.stripe} />
+      <View style={[styles.stripe, styles.greenStripe]} />
+    </View>
+  );
+};
+
+
 
 const HomeScreen = () => {
   // <--Le cambie la funcion a una flecha por que es mas bonito ;)
   const navigation = useNavigation();
-  return  (
-      <ImageBackground
-        source={require("../assets/R.jpg")}
-        style={styles.backgroundImage}
-      >
-        <TouchableOpacity  onPress={() => {
-          navigation.navigate("CienciasScreen")
-        }}  style={styles.button} activeOpacity={0.7}>
-          <Image
-            source={require("../assets/ciencias.jpg")}
-            style={styles.buttonImage}
-          />
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("CienciasScreen");
+          }}
+          style={styles.button}
+          activeOpacity={0.7}
+        >
           <Text style={styles.buttonText}>Ciencias</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button1} activeOpacity={0.7}>
-          <Image
-            source={require("../assets/biolo.jpg")}
-            style={styles.buttonImage1}
-          />
           <Text style={styles.buttonText1}>Biología</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.button2} activeOpacity={0.7}>
-          <Image
-            source={require("../assets/mate.jpg")}
-            style={styles.buttonImage2}
-          />
           <Text style={styles.buttonText2}>Matemáticas</Text>
         </TouchableOpacity>
-      </ImageBackground>
+      </View>
+      <View/>
+      <LinearGradient
+          colors={["#B7E4C7", "#A5D6A7"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.gradientBackground}
+    />
+    </View>
   );
 };
 
@@ -56,46 +65,17 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   mainContainer: {
-    backgroundColor: "#f1f1f1",
     flex: 1,
   },
   container: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  containerSVG: {
-    width: width,
-    justifyContent: "flex-start",
-    alignItems: "center",
-  },
-  titulo: {
-    fontSize: 80,
-    color: "#34434D",
-    fontWeight: "bold",
-  },
-  subTitle: {
-    fontSize: 20,
-    color: "gray",
-  },
-  textInput: {
-    padding: 10,
-    paddingStart: 30,
-    width: "80%",
-    height: 50,
-    marginTop: 20,
-    borderRadius: 30,
-    backgroundColor: "#fff",
-  },
-  forgotPassword: {
-    fontSize: 14,
-    color: "gray",
-    marginTop: 11,
-  },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: "cover",
     alignItems: "flex-start",
     justifyContent: "center",
+    flex:1,
+    zIndex:2,
+  },
+  gradientBackground: {
+    ...StyleSheet.absoluteFillObject, 
+    zindex:1,
   },
   button: {
     width: 350,
@@ -105,9 +85,10 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 15,
-    backgroundColor: "#ccc", // Color de fondo del botón
-    shadowColor: "#000", // Color de la sombra
+    borderRadius: 20,
+    borderColor:'white',
+    backgroundColor: "#B7E4C7", // Color de fondo del botón
+    shadowColor: "black", // Color de la sombra
     shadowOffset: {
       width: 0,
       height: 2,
@@ -115,12 +96,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 3,
     elevation: 3,
-  },
-  buttonImage: {
-    width: "100%",
-    height: "100%",
-    opacity: 1,
-    borderRadius: 15,
   },
   buttonText: {
     position: "absolute",
@@ -136,8 +111,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 15,
-    backgroundColor: "#ccc", // Color de fondo del botón
+    borderRadius: 20,
+    backgroundColor: "#B7E4C7", // Color de fondo del botón
     shadowColor: "#000", // Color de la sombra
     shadowOffset: {
       width: 0,
@@ -146,13 +121,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.3,
     shadowRadius: 3,
     elevation: 5,
-  },
-  buttonImage1: {
-    ...StyleSheet.absoluteFillObject,
-    width: "100%",
-    height: "100%",
-    borderRadius: 15,
-    opacity: 1,
   },
   buttonText1: {
     position: "absolute",
@@ -168,8 +136,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 15,
-    backgroundColor: "#ccc", // Color de fondo del botón
+    borderRadius: 20,
+    backgroundColor: "#B7E4C7", // Color de fondo del botón
     shadowColor: "#000", // Color de la sombra
     shadowOffset: {
       width: 0,
@@ -179,13 +147,7 @@ const styles = StyleSheet.create({
     shadowRadius: 3,
     elevation: 8,
   },
-  buttonImage2: {
-    ...StyleSheet.absoluteFillObject,
-    width: "100%",
-    height: "100%",
-    borderRadius: 15,
-    opacity: 1,
-  },
+
   buttonText2: {
     position: "absolute",
     color: "#f1f1f1", // Color del texto
